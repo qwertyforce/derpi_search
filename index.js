08b967178e1f34301ed3c50734231ece0f241ad6
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-06-22 16:44:43 +0300

    derpicdn support

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const axios = require('axios')
 const DERPI_API_KEY = "DERPI_API_KEY"
 
 const client = new Client();
-async function process_response(message, response) {
+async function process_response(message, img_id) {
     const author_id = message.author.id
-    if (response.data.images.length > 0) {
-        const derpi_url = `https://derpibooru.org/images/${response.data.images[0].id}`;
+    if (img_id!==undefined) {
+        const derpi_url = `https://derpibooru.org/images/${img_id}`;
         message.channel.send(`<@!${author_id}> ${derpi_url}`);
     } else {
         message.channel.send(`<@!${author_id}> Not found 😢`);
@@ -27,6 +27,13 @@ client.on('message', async (message) => {
         try {
             let msg = await message.channel.messages.fetch(msg_id)
             msg.embeds.forEach(async (el) => {
+                if(el.url.includes("https://derpicdn.net")){   //https://derpicdn.net/img/view/{year}/{month}/{day}/2378301.gif
+                   const index_of_last_slash =  el.url.lastIndexOf("/") 
+                   const index_of_last_dot =  el.url.lastIndexOf(".") 
+                   const img_id=el.url.slice(index_of_last_slash+1,index_of_last_dot)
+                   process_response(message, img_id)
+                   return
+                }
                 if (el.url.includes("https://imgur.com/")) {
                     const index = el.url.indexOf("https://imgur.com/")
                     let img_id = el.url.slice(index + 18)
@@ -36,12 +43,12 @@ client.on('message', async (message) => {
                     el.url = "https://i.imgur.com/" + img_id
                 }
                 const response = await axios.post(`https://derpibooru.org/api/v1/json/search/reverse?key=${DERPI_API_KEY}&url=${el.url}`);
-                process_response(message, response)
+                process_response(message, response.data.images[0]?.id)
             })
 
             msg.attachments.forEach(async (el) => {
                 const response = await axios.post(`https://derpibooru.org/api/v1/json/search/reverse?key=${DERPI_API_KEY}&url=${el.attachment}`);
-                process_response(message, response)
+                process_response(message, response.data.images[0]?.id)
             })
         } catch (err) {
             console.log(err)
